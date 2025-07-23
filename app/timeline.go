9b80459d689466c1b0250316b7f0613de70d3893
makachanm <maka8926jp@gmail.com>
2025-07-23 15:52:29 +0900

    (add): auto-refresh

--- app/timeline.go
+++ app/timeline.go
@@ -7,6 +7,7 @@ import (
 	"termfedi/config"
 	"termfedi/layer"
 	"termfedi/utils"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 )
@@ -82,6 +83,9 @@ func NewTimelineScreen(config config.Configuration) *TimelineScreen {
 func (ts *TimelineScreen) InitScene(screen tcell.Screen, ctx ApplicationContext) {
 	_, h := screen.Size()
 	ts.Timelines.SetMaxItemPerPage(int(h / 6))
+
+	autoRef := func() { ts.autoRefresh(screen, ctx) }
+	time.AfterFunc(time.Second*30, autoRef)
 }
 
 func (ts *TimelineScreen) WindowChangedScene(screen tcell.Screen, ctx ApplicationContext) {
@@ -144,6 +148,13 @@ func (ts *TimelineScreen) refreshData(screen tcell.Screen, ctx ApplicationContex
 	screen.Clear()
 	ts.drawNotes(screen, ctx)
 
+}
+
+func (ts *TimelineScreen) autoRefresh(screen tcell.Screen, ctx ApplicationContext) {
+	ts.refreshData(screen, ctx)
+
+	autoRef := func() { ts.autoRefresh(screen, ctx) }
+	time.AfterFunc(time.Second*30, autoRef)
 }
 
 func (ts *TimelineScreen) drawNotes(screen tcell.Screen, ctx ApplicationContext) {
